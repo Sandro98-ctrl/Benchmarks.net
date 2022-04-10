98e68171b542a9d79f24f748d2a064c4f7a277b0
snyland <sandronyland98@gmail.com>
2022-04-09 22:00:42 -0300

    Adding HashIds Benchmark

---
+++ src/HashIds.Benchmark/HashIdBenchmark.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using HashidsNet;
+
+namespace HashIds.Benchmark;
+
+[MemoryDiagnoser(false)]
+public class HashIdBenchmark
+{
+    private static readonly Hashids HashIds = new("XC@s*mg9GBHA", 11);
+
+    [Benchmark]
+    public int IntFromHashArray()
+    {
+        return HashIds.Decode("KpbonNyVje7")[0];
+    }
+
+    [Benchmark]
+    public int IntFromHashSingle()
+    {
+        return HashIds.DecodeSingle("KpbonNyVje7");
+    }
+
+    [Benchmark]
+    public string HashFromInt()
+    {
+        return HashIds.Encode(1);
+    }
+}
---
+++ src/HashIds.Benchmark/Program.cs
@@ -0,0 +1,28 @@
+﻿#if !DEBUG
+
+using BenchmarkDotNet.Running;
+using HashIds.Benchmark;
+
+BenchmarkRunner.Run<HashIdBenchmark>();
+
+#else
+
+using HashidsNet;
+
+var hashIds = new Hashids("XC@s*mg9GBHA", 11);
+
+var firstId = hashIds.Encode(1);
+var secondIds = hashIds.Encode(2, 3);
+
+Console.WriteLine(firstId);
+Console.WriteLine(secondIds);
+
+var firstRawId = hashIds.Decode("KpbonNyVje7");
+var firstRawSingleId = hashIds.DecodeSingle("KpbonNyVje7");
+var secondRawIds = hashIds.Decode("EdxZW1H8V1A");
+
+Console.WriteLine(firstRawId[0]);
+Console.WriteLine(firstRawSingleId);
+Console.WriteLine(string.Join(", ", secondRawIds));
+
+#endif
